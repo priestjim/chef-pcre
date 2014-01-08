5cc27e785513f252ae60b0c780f04995e4b63776
Panagiotis Papadomitsos <pj@bugsense.com>
2014-01-08 11:32:53 +0200

    Version bump. Fixed create_if_missing

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,9 +17,9 @@
 # limitations under the License.
 #
 
-default['pcre']['version']      = '8.33'
+default['pcre']['version']      = '8.34'
 default['pcre']['url']          = "http://sourceforge.net/projects/pcre/files/pcre/#{node['pcre']['version']}/pcre-#{node['pcre']['version']}.tar.bz2/download"
-default['pcre']['checksum']     = 'c603957a4966811c04af5f6048c71cfb4966ec93312d7b3118116ed9f3bc0478'
+default['pcre']['checksum']     = 'b6043ae1ff2720be665ffa28dc22b7c637cdde96f389a116c0c3020caeae583f'
 default['pcre']['prefix']       = '/opt'
 
 default['pcre']['add_ldconfig'] = false
